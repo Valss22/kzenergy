09f31373d7cead70871ad4f9f188142a08ad90c8
Valss22 <valsshokorov@gmail.com>
2021-06-27 12:36:09 +0600

    add cloud for excel

--- backend/models.py
+++ backend/models.py
@@ -9,7 +9,7 @@ def json_default():
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE, null=True)
-    excel = CloudinaryField(resource_type='auto')
+    excel = CloudinaryField(resource_type='auto', null=True)
 
     def __str__(self):
         return f'{self.user}({self.id})'
