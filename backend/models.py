34d4d377f81b8e2f95da38c6db796478270039d5
Valss22 <valsshokorov@gmail.com>
2021-07-15 00:38:51 +0600

    fix 2froup q

--- backend/models.py
+++ backend/models.py
@@ -34,14 +34,14 @@ class Gas(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.CharField(default=None, max_length=50, null=True)
     gasName = models.CharField(max_length=50, null=True)
-    density = models.FloatField(help_text='плотность газа', null=True)
     nitrogen = models.FloatField(help_text='% масс N', null=True)
     sulfur = models.FloatField(help_text='% масс S', null=True)
     carbon = models.FloatField(help_text='% масс C', null=True)
-    LowerHeatCombustion = models.FloatField(help_text='ТГ, ГДж/т', null=True)
+    density = models.FloatField(help_text='плотность газа', null=True)
     CO2EmissionFactor = models.FloatField(help_text='тСО2/ТДж', null=True)
     CH4SpecificFactor = models.IntegerField(help_text='кг/ТДж', null=True)
     N2OSpecificFactor = models.IntegerField(help_text='кг/ТДж', null=True)
+    LowerHeatCombustion = models.FloatField(help_text='ТГ, ГДж/т', null=True)
 
     def __str__(self):
         return f'{self.gasName}({self.id})'
