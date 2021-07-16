15c1e66fe18564a7ae4b6415e2b2bdfc4cf5f8f4
Valss22 <valsshokorov@gmail.com>
2021-07-16 20:18:57 +0600

    fix naming

--- backend/services/common.py
+++ backend/services/common.py
@@ -9,6 +9,7 @@ def get_refusal_data(request) -> dict:
     date = parse_date(str(datetime.datetime.now()))
     refusalData['date'] = date
     currentUser = get_current_user(request)
-    user = {'fullName': currentUser.fullName}
+    user = {'fullName': currentUser.fullName,
+            'id': currentUser.id}
     refusalData['user'] = user
     return refusalData
