b720c85bdc451595c986706d6ab7bae1b363ff58
Valss22 <valsshokorov@gmail.com>
2021-08-31 22:54:16 +0600

    multy parser

--- backend/services/user_profile.py
+++ backend/services/user_profile.py
@@ -14,7 +14,7 @@ def update_profile(request):
 
 def update_avatar(request):
     current_user = get_current_user(request)
-    changed_avatar = cloudinary.uploader.upload(request.data['avatar'])
+    changed_avatar = cloudinary.uploader.upload(request.data['avatar']['data'])
     current_user.avatar = changed_avatar
     current_user.save()
     serializer = AvatarSerializer(current_user)
